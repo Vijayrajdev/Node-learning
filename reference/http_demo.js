7b73e27ec7a4a7dfb81a8c55d4a396278ae03c8c
Vijayraj-wd <vijayrajck@outlook.com>
2021-09-05 17:18:55 +0530

    Server added

---
+++ reference/http_demo.js
@@ -0,0 +1,11 @@
+const http = require("http");
+
+// Create server
+
+http
+  .createServer((req, res) => {
+    // Write response
+    res.write("Hello World!");
+    res.end();
+  })
+  .listen(5000, () => console.log("Server running..."));
